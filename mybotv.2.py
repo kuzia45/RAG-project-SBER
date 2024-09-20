64a803270f9470c1b23e247f7aa562bf5dab3766
unknown <kuzia45@gmail.com>
2024-09-20 11:47:01 +0300

    added document function to telebot. No add_retriever

--- mybotv.2.py
+++ mybotv.2.py
@@ -24,32 +24,44 @@ def get_file_names():
 # Функция для автоматического ответа в случае нетекстового сообщения
 @bot.message_handler(content_types=['audio',
                                     'video',
-                                    'document',
                                     'photo',
                                     'sticker',
                                     'voice',
                                     'location',
                                     'contact'])
 def not_text(message):
-  user_id = message.chat.id
-  bot.send_message(user_id, 'Я работаю только с текстовыми сообщениями!')
+    user_id = message.chat.id
+    bot.send_message(message.from_user.id, 'Я работаю только с текстовыми сообщениями!')
+#Добавление документа
+@bot.message_handler(content_types=['document'])
+def add_documnet(document):
+    try:
+        file_info = bot.get_file(document.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        bot.send_message (document.from_user.id, f'Вы загрузили следующий файл {document.document.file_name}')
+    except Exception as e:
+        bot.send_message(document.from_user.id, e)
+        print (document)
 #Кнопка старт
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = types.KeyboardButton('Очистить историю')
+button2 = types.KeyboardButton('Информация о боте')
+button3 = types.KeyboardButton('Добавить документ')
+markup.add(button1, button2, button3)
 @bot.message_handler(commands=['help', 'start'], content_types=['text'])
 def send_welcome(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button1 = types.KeyboardButton('Очистить историю')
-    button2 = types.KeyboardButton('Информация о боте')
-    markup.add(button1, button2)
     bot.send_message(message.from_user.id, """Привет. Я RAG-бот. В меня загружены документы и я могу по ним ответить на твой вопрос""", reply_markup=markup)
 
 # Функция, обрабатывающая текстовые сообщения
 @bot.message_handler(content_types=['text'])
 def answer_the_question(message):
     user_id = message.chat.id
+    #Обработка кнопок
     if message.text == 'Информация о боте':
         bot.send_message(message.from_user.id, f'Я RAG-бот. Я могу ответить на любой вопрос, связанный со следующими документами \n\n {get_file_names()}')
-    else:
-        # Получение и отправка ответа через GigaChat
+    elif message.text == 'Добавить документ':
+        bot.send_message(message.from_user.id, 'Прикрепите документ и отправьте мне. Документ должен быть формата PDF')
+    else:# Получение и отправка ответа через GigaChat
         response = conversational_rag_chain.invoke({'input': message.text}, config={
                             "configurable": {"session_id": user_id}
                         })
